410aaecc24b728b60a1727b683d5d5b673d518f1
Fi1imon <angelo4ekfilippof@gmail.com>
2023-06-14 01:17:05 +0300

    1. Сделал функционал выгрузки карточек 2. Реализовал открытие попапа с изображением карточки

---
+++ src/utils/api.js
@@ -0,0 +1,92 @@
+class Api {
+  constructor({baseUrl, headers}) {
+    this._baseUrl = baseUrl;
+    this._hraders = headers;
+  }
+
+  _isOk = (res) => {
+    if(res.ok) {
+      return res.json();
+    }
+
+    return  Promise.reject(`Ошибка: ${res.status}`)
+  }
+
+  _request(url, options) {
+    return fetch(`${this._baseUrl}/${url}`, options).then(this._isOk)
+  }
+
+  getUserInfo() {
+    return  this._request('users/me', {
+      headers: this._hraders
+    })
+  }
+
+  getInitialCards() {
+    return  this._request('cards', {
+      headers: this._hraders
+    })
+  }
+
+  setUserInfo(values) {
+    return  this._request('users/me', {
+      method: 'PATCH',
+      headers: this._hraders,
+      body: JSON.stringify({
+        name: values.name,
+        about: values.job
+      })
+    })
+  }
+
+  addCard(values) {
+    return  this._request('cards', {
+      method: 'POST',
+      headers: this._hraders,
+      body: JSON.stringify({
+        name: values['title'],
+        link: values['url']
+      })
+    })
+  }
+
+  deleteCard(cardId) {
+    return  this._request(`cards/${cardId}`, {
+      method: 'DELETE',
+      headers: this._hraders
+    })
+  }
+
+  setLike(cardId) {
+    return  this._request(`cards/${cardId}/likes`, {
+      method: 'PUT',
+      headers: this._hraders
+    })
+  }
+
+  removeLike(cardId) {
+    return  this._request(`cards/${cardId}/likes`, {
+      method: 'DELETE',
+      headers: this._hraders
+    })
+  }
+
+  uploadAvatar(imageUrl) {
+    return  this._request('users/me/avatar', {
+      method: 'PATCH',
+      headers: this._hraders,
+      body: JSON.stringify({
+        avatar: imageUrl
+      })
+    })
+  }
+}
+
+const api = new Api({
+  baseUrl: 'https://nomoreparties.co/v1/cohort-66/',
+  headers: {
+    authorization: '1b376021-e410-4f6f-96c7-0ea2e231f4c4',
+    'Content-Type': 'application/json'
+  }
+})
+export default api
